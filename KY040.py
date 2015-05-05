6e8260fda18c33721644ccb69a782416eb4c4e0b
martinohanlon <martin@ohanlonweb.com>
2015-05-05 22:03:57 +0100

    first commit
    initial version

---
+++ KY040.py
@@ -0,0 +1,68 @@
+#KY040 Python Class
+#Martin O'Hanlon
+#stuffaboutcode.com
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+class KY040:
+
+    CLOCKWISE = 0
+    ANTICLOCKWISE = 1
+    
+    def __init__(self, clockPin, dataPin, switchPin, rotaryCallback, switchCallback):
+        #persist values
+        self.clockPin = clockPin
+        self.dataPin = dataPin
+        self.switchPin = switchPin
+        self.rotaryCallback = rotaryCallback
+        self.switchCallback = switchCallback
+
+        #setup pins
+        GPIO.setup(clockPin, GPIO.IN)
+        GPIO.setup(dataPin, GPIO.IN)
+        GPIO.setup(switchPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    def start(self):
+        GPIO.add_event_detect(self.clockPin, GPIO.FALLING, callback=self._clockCallback, bouncetime=250)
+        GPIO.add_event_detect(self.switchPin, GPIO.FALLING, callback=self._switchCallback, bouncetime=300)
+
+    def stop(self):
+        GPIO.remove_event_detect(self.clockPin)
+        GPIO.remove_event_detect(self.switchPin)
+    
+    def _clockCallback(self, pin):
+        data = GPIO.input(self.dataPin)
+        if data == 1:
+            self.rotaryCallback(self.ANTICLOCKWISE)
+        else:
+            self.rotaryCallback(self.CLOCKWISE)
+
+    def _switchCallback(self, pin):
+        if GPIO.input(self.switchPin) == 0:
+            self.switchCallback()
+
+#test
+if __name__ == "__main__":
+    
+    CLOCKPIN = 5
+    DATAPIN = 6
+    SWITCHPIN = 13
+
+    def rotaryChange(direction):
+        print "turned - " + str(direction)
+    def switchPressed():
+        print "button pressed"
+
+    GPIO.setmode(GPIO.BCM)
+    
+    ky040 = KY040(CLOCKPIN, DATAPIN, SWITCHPIN, rotaryChange, switchPressed)
+
+    ky040.start()
+
+    try:
+        while True:
+            sleep(0.1)
+    finally:
+        ky040.stop()
+        GPIO.cleanup()
---
+++ README.md
@@ -0,0 +1,9 @@
+#KY040 Python Module 
+##Martin O'Hanlon (martin@ohanlonweb.com)
+##http://www.stuffaboutcode.com
+
+##Description
+A python module for reading the values from a KY040 rotary encoder module using a raspberry pi [http://www.stuffaboutcode.com/2015/05/raspberry-pi-and-ky040-rotary-encoder.html]([http://www.stuffaboutcode.com/2015/05/raspberry-pi-and-ky040-rotary-encoder.html])
+
+##Version history
+* 0.1 - Initial stable version
